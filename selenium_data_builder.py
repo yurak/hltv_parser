8375e70040f51b21ab34889387c40e96ebac02c6
Yura Kuzhiy <yurakuzhiy@gmail.com>
2025-03-12 18:35:59 +0200

    change selenium builder py

--- selenium_data_builder.py
+++ selenium_data_builder.py
@@ -6,6 +6,7 @@ class SeleniumDataBuiler:
     def __init__(self, driver, cs_map):
         self.driver = driver
         self.cs_map = cs_map
+        self.dict = {}
 
     def player_name(self):
         return self.driver.find_element(By.XPATH, "//h1[contains(@class, 'summaryNickname')]").text
@@ -17,51 +18,34 @@ class SeleniumDataBuiler:
         age_str = element = self.driver.find_element(By.CLASS_NAME, "summaryPlayerAge").text
         age = int(age_str.split()[0])
         return age
-
     
+    def kpr(self):
+        parent_div = self.driver.find_element(By.CSS_SELECTOR, "div.role-stats-row.stats-side-combined[data-per-round-title='Kills per round']")
+        return float(parent_div.get_attribute("data-original-value"))
+
     def build(self):
-        dict = self.main_section()
-        dict['player_name'] = self.player_name()
-        dict['map'] = self.cs_map
-        dict['age'] = self.age()
+        self.section('', True)
+        self.dict['player_name'] = self.player_name()
+        self.dict['map'] = self.cs_map
+        self.dict['age'] = self.age()
         element = self.driver.find_element(By.XPATH, '//*[@data-side-stats="ct"]')
         element.click()
-        dict_ct =  self.main_section_ct()
-        dict.update(dict_ct)
+        self.section('ct_')
         element = self.driver.find_element(By.XPATH, '//*[@data-side-stats="t"]')
         element.click()
-        dict_t =  self.main_section_t()
-        dict.update(dict_t)
-        return dict
-        
-    def main_section(self):
-        allow_cookies_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Allow all cookies')]"))
-        )
-        allow_cookies_button.click()
-     
-        elements =  self.driver.find_elements(By.CLASS_NAME, 'role-stats-section')
-        dict = {}
-        for element in elements:
-            array = element.text.split('\n')
-            dict['all_'+array[0].lower()] = int(array[1])/100
-        
-        return dict
+        self.section('t_')
+        return self.dict
     
-    def main_section_ct(self):
+    def section(self, side ='', click_cookie=False ):
+        if click_cookie:
+            allow_cookies_button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Allow all cookies')]"))
+            )
+            allow_cookies_button.click()
+        
         elements =  self.driver.find_elements(By.CLASS_NAME, 'role-stats-section')
         dict = {}
         for element in elements:
             array = element.text.split('\n')
-            dict['ct_' + array[0].lower()] = int(array[1])/100
-        
-        return dict
-    
-    def main_section_t(self):
-        elements =  self.driver.find_elements(By.CLASS_NAME, 'role-stats-section')
-        dict = {}
-        for element in elements:
-            array = element.text.split('\n')
-            dict['t_' + array[0].lower()] = int(array[1])/100
-        
-        return dict
+            property_name = side + array[0].lower()
+            self.dict[property_name] = int(array[1])/100
